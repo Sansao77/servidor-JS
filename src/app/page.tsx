ae688fd0e02723a93682b23c7ebd4b49a286e521
Gabriel <gabrielp_santos@hotmail.com>
2023-12-11 16:30:22 -0300

    feat: add new handle functions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,76 @@
 "use client"
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { api } from '../../services/api';
 
-const initialItems = [
-  {id: 1, nome: "Maça"},
-  {id: 2, nome: "Uva"},
-]
+interface Product{
+  id: number;
+  nome: string;
+}
 
 export default function Home() {
-  const [itens, setItems] = useState(initialItems);
+  const [loading, setLoading] = useState(false);
+  const [itens, setItens] = useState<Product[]>([]);
   const [textInput, setTextInput] = useState('');
 
-  async function handleClick(){
+  /*async function handleClick(){
     const response = await api.get("/produtos");
     console.log(response);
-    setItems(response.data);
+    setItens(response.data);
 
-    //const response = await fetch("http://192.168.68.154:3000/produtos");
+    //const response = await fetch("http://192.168.68.153:3000/produtos");
     //const produtos = await response.json();
-  }
+  }*/
 
   function handleClickFilter(){
     // filtra todos os elementos que tem index impar (só esses ficam guardados no array)
-    setItems(itens.filter(n=>(n.id-1)%2 !== 0));
+    setItens(itens.filter(n=>(n.id%2) !== 0));
     console.log(itens);
   }
 
-  async function handleCLickItem(){
+  useEffect(()=>{
+    loadItems();
+  }, []);// O array defini quais efeitos devem ser feitos para atualizar a página
+
+  useEffect(()=>{
+    console.log("O código está passando por aqui");
+  }, []);
+
+  /**
+   * Esta função serve para carregar a página depois de suas alterações, sendo a adição
+   */
+  async function loadItems(){
+    setLoading(true);
+    setTimeout(async() => {
+      try{
+        const response = await api.get("/produtos");
+        console.log(response);
+        setItens(response.data);
+      }
+      catch(error){
+        console.error("Error: ", error);
+      }
+      finally{
+        setLoading(false);
+      }
+    }, 2000);
+  }
+  /**
+   * 
+   */
+  async function handleCLickAddItem(){
     console.log(textInput);
     const data = {nome: textInput};
-
+    loadItems();
     try{
-      const response = await fetch("http://192.168.68.154:3000/produtos",{
+      if(textInput === "") throw Error;
+      await api.post("/produtos", data);
+    }
+    catch(error){
+      console.error("Error: ", error);
+    }
+    
+    /*try{
+      const response = await fetch("http://192.168.68.153:3000/produtos",{
         method: "Post", //or Put
         headers: {
           "Content-Type": "application/json",
@@ -44,33 +83,52 @@ export default function Home() {
     catch(error){
       console.error("Error: ", error);
       alert("Ocorreu um erro");
-    }
+    }*/
   }
-
-  function handleClickDeleteItem(){
-    console.log(textInput);
-    setItems(itens.filter(x => x.nome !== textInput));
+  /**
+   * Essa função deleta o elemento ao lado dele e retorna a lista sem o elemento
+   * @param itemId representa o Id do elemento que sera deletado
+   */
+  function handleClickDeleteItem(itemId:number){
+    console.log("Deletar elemento de ID: ", itemId);
+    setTimeout(async() => {
+      try{
+        const response = await api.delete(`/produtos/${itemId}`);
+        console.log(response);
+      }
+      catch(error){
+        console.error("Error: ", error);
+      }
+      finally{
+        setLoading(false);
+      }
+    }, 1000);
+    loadItems();
   }
 
   return (
    <main>
       <div style={{marginBottom: 10}}>
-        {/**Esse 'onChange' permite pegar*/}
-        <input onChange={(e)=> setTextInput(e.target.value)} type="text" placeholder='Digite seu texto aqui'/>
-        <button onClick={handleCLickItem}>Enviar</button>
-        <button onClick={handleClickDeleteItem}>Deletar</button>
+        {/**Esse 'onChange' permite pegar o texto do input*/}
+        <input onChange={e=>setTextInput(e.target.value)} type="text" placeholder='Digite seu texto aqui'/>
+        <button onClick={handleCLickAddItem}>Enviar</button>
       </div>
 
-      <button type='button' onClick={handleClick}>
-        Buscar informações no servidor
-      </button>
+      <span>{loading && "Carregando..."}</span>
+
+      {/*<button type='button' onClick={handleClick}>
+        Buscar informações no servidor </button>*/}
+
       <button type='button' onClick={handleClickFilter}>
         Filtrar os elementos
       </button>
 
       <ul>
       {itens.map(item =>(
-        <li key={item.id}> {item.nome}</li>
+        <li key={item.id}> 
+        {item.nome}
+        <button style={{marginLeft: 10}}onClick={()=>{handleClickDeleteItem(item.id)}}> Delete </button>
+        </li>
       ))}
       </ul>
    </main>
